e06b2adfd1eda916ffb68cdb9b74c264d3915d9c
GNU Shim <geunwoo.j.shim@gmail.com>
2020-06-10 06:49:56 +0900

    Create blue-green.sh

---
+++ blue-green.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "----- before deploy"
+export deploy=$1
+cd deployment
+kustomize edit add label deploy:$deploy -f
+kustomize edit set namesuffix -- -$deploy
+kustomize edit set image 192.168.1.101:8443/echo-buildtime:$deploy
+kustomize build . | kubectl apply -f -
+echo "----- after new deploy"
+kubectl get deployment -o wide
+while true;
+do
+export replicas=$(kubectl get deployment --selector=app=echo-buildtime,deploy=$deploy -o jsonpath --template="{.items[0].status.replicas}")
+export ready=$(kubectl get deployment --selector=app=echo-buildtime,deploy=$deploy -o jsonpath --template="{.items[0].status.readyReplicas}")
+echo "replicas: $replicas, readyReplicas: $ready"
+if [ "$ready" == "$replicas" ]; then
+  echo "switching new deployment"
+  cd ../service
+  kustomize edit add label deploy:$deploy -f
+  kustomize build . | kubectl apply -f -
+  sleep 3
+  kubectl delete deployment --selector=app=echo-buildtime,deploy!=$deploy
+  echo "----- after old deploy remove"
+  kubectl get deployment -o wide
+  break
+else
+  sleep 1
+fi
+done
