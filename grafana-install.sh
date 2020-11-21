f84e73ee35e588e5f2b7ce5131b81c26d79a8d3d
GNU Shim <geunwoo.j.shim@gmail.com>
2020-11-21 16:31:59 +0900

    Create grafana-install.sh

---
+++ grafana-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+helm install grafana \
+--set persistence.enabled=true \
+--set persistence.existingClaim=grafana \
+--set service.type=LoadBalancer \
+--set tolerations[0].key=node-role.kubernetes.io/master \
+--set tolerations[0].effect=NoSchedule \
+--set tolerations[0].operator=Exists \
+--set nodeSelector."kubernetes\.io/hostname"=m-k8s \
+edu/grafana
